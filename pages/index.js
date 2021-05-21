b8c0fbfd26dc19aabe63ed4282d08d64b6de71ef
kazuki <kaz.tokyo.tech20@gmail.com>
2021-05-21 09:38:58 +0900

    footerのcomponent化に伴った微調整

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import footer from '../styles/Footer.module.css'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
