08558a5e930d1f276e5fce6ec964c00c0dae933e
kazuki <kaz.tokyo.tech20@gmail.com>
2021-05-21 09:58:49 +0900

    矢印を削除

--- pages/index.js
+++ pages/index.js
@@ -18,48 +18,48 @@ export default function Home() {
 
         <div className={styles.grid}>
           <div className={styles.card}>
-            <h2 className={styles.border}>教養科目 &rarr;</h2>
+            <h2 className={styles.border}>教養科目 </h2>
             <p className={styles.back}>文系教養科目</p>
             <p className={styles.back}>英語科目</p>
             <p className={styles.back}>第二外国語科目</p>
           </div>
 
           <div className={styles.card}>
-            <h2 className={styles.border}>初年次専門科目 &rarr;</h2>
+            <h2 className={styles.border}>初年次専門科目 </h2>
             <p className={styles.back}>理学院</p>
             <p className={styles.back}>工学院</p>
             <p className={styles.back}>物質理工学院</p>
           </div>
 
           <div className={styles.card}>
-            <h2 className={styles.border}>理学院 &rarr;</h2>
+            <h2 className={styles.border}>理学院 </h2>
             <p className={styles.back}>数学系</p>
             <p className={styles.back}>物理学系</p>
             <p className={styles.back}>化学系</p>
           </div>
 
           <div className={styles.card}>
-            <h2 className={styles.border}>工学院 &rarr;</h2>
+            <h2 className={styles.border}>工学院 </h2>
             <p></p>
           </div>
 
           <div className={styles.card}>
-            <h2 className={styles.border}>物質理工学院 &rarr;</h2>
+            <h2 className={styles.border}>物質理工学院 </h2>
             <p></p>
           </div>
 
           <div className={styles.card}>
-            <h2 className={styles.border}>情報理工学院 &rarr;</h2>
+            <h2 className={styles.border}>情報理工学院 </h2>
             <p></p>
           </div>
 
           <div className={styles.card}>
-            <h2 className={styles.border}>環境社会理工学院 &rarr;</h2>
+            <h2 className={styles.border}>環境社会理工学院 </h2>
             <p></p>
           </div>
 
           <div className={styles.card}>
-            <h2 className={styles.border}>生命理工学院 &rarr;</h2>
+            <h2 className={styles.border}>生命理工学院 </h2>
             <p></p>
           </div>
         </div>
